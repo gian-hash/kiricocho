1098ce2b8cd78326489f79c632aa72015926e11a
Santagata <gianmarco.santagata@accenture.com>
2026-06-03 22:27:20 +0200

    resize image

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const path = require('path');
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use('/api/auth', require('./src/routes/auth'));
